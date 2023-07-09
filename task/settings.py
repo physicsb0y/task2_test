ea2a4ecc8e13e7270fd4e71727baecf3389667d8
physicsb0y <sagar1.735401@trc.tu.edu.np>
2023-07-09 16:09:02 +0545

    Solving static files issue

--- task/settings.py
+++ task/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = str(BASE_DIR / 'static')
 
 #Media files
 
